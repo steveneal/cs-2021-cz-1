174dc51842741e73b782c98bd1ae7b02c1a3f3b2
CharlesPerriard <charles.perriard@credit-suisse.com>
2021-07-08 07:30:40 -0400

    1st commit (testing git + TradeDataLoader)

--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -3,7 +3,10 @@ package com.cs.rfq.decorator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import java.util.Arrays;
 
 public class RfqDecoratorMain {
 
@@ -12,12 +15,25 @@ public class RfqDecoratorMain {
         System.setProperty("spark.master", "local[4]");
 
         //TODO: create a Spark configuration and set a sensible app name
+        SparkConf conf = new SparkConf().setAppName("StreamFromFile");
 
         //TODO: create a Spark streaming context
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         //TODO: create a Spark session
+//        SparkSession spark = SparkSession
+//                .builder()
+//                .master("local")
+//                .appName("JavaALSExample")
+//                .getOrCreate();
 
         //TODO: create a new RfqProcessor and set it listening for incoming RFQs
+        JavaDStream<String> lines = jssc.textFileStream("src/test/resources/trades");
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+        words.print();
+
+        jssc.start();
+        jssc.awaitTermination();
     }
 
 }
--- src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
+++ src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
@@ -19,13 +20,22 @@ public class TradeDataLoader {
 
     public Dataset<Row> loadTrades(SparkSession session, String path) {
         //TODO: create an explicit schema for the trade data in the JSON files
-        StructType schema = null;
+        StructType schema = new StructType(new StructField[]{
+                new StructField("TraderId", LongType, false, Metadata.empty()),
+                new StructField("EntityId", LongType, false, Metadata.empty()),
+                new StructField("SecurityID", StringType, false, Metadata.empty()),
+                new StructField("LastQty", LongType, false, Metadata.empty()),
+                new StructField("LastPx", DoubleType, false, Metadata.empty()),
+                new StructField("TradeDate", DateType, false, Metadata.empty()),
+                new StructField("Currency", StringType, false, Metadata.empty())
+        });
 
         //TODO: load the trades dataset
-        Dataset<Row> trades = null;
+        Dataset<Row> trades = session.read().schema(schema).json(path);
+
 
         //TODO: log a message indicating number of records loaded and the schema used
-
+        System.out.println(trades.count());
         return trades;
     }
 
