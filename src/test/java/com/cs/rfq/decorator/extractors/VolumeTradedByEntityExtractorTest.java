673e0fc79b95793a22574026248ab54f68c4f96c
Rati <ratanak.hy@credit-suisse.com>
2021-07-09 04:45:44 -0400

    Added total volume traded by Entity extractor and respective tests

--- src/test/java/com/cs/rfq/decorator/extractors/VolumeTradedByEntityExtractorTest.java
+++ src/test/java/com/cs/rfq/decorator/extractors/VolumeTradedByEntityExtractorTest.java
@@ -1,4 +1,92 @@
 package com.cs.rfq.decorator.extractors;
 
-public class VolumeTradedWithEntityExtractorTest {
+import com.cs.rfq.decorator.Rfq;
+import com.cs.rfq.decorator.TradeDataLoader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class VolumeTradedByEntityExtractorTest extends AbstractSparkUnitTest {
+
+    private Rfq rfq;
+
+    @Before
+    public void setup() {
+        rfq = new Rfq();
+        rfq.setEntityId(4461279226039677777L);
+    }
+
+    @Test
+    public void checkVolumeTradedByEntityPastWeek() {
+
+        String filePath = getClass().getResource("volume-traded-by-entity.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        VolumeTradedByEntityExtractor extractor = new VolumeTradedByEntityExtractor();
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastWeek);
+
+        assertEquals(180L, result);
+    }
+
+    @Test
+    public void checkVolumeTradedByEntityPastMonth() {
+
+        String filePath = getClass().getResource("volume-traded-by-entity.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        VolumeTradedByEntityExtractor extractor = new VolumeTradedByEntityExtractor();
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastMonth);
+
+        assertEquals(240L, result);
+    }
+
+    @Test
+    public void checkVolumeTradedByEntityPastYear() {
+
+        String filePath = getClass().getResource("volume-traded-by-entity.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        VolumeTradedByEntityExtractor extractor = new VolumeTradedByEntityExtractor();
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastYear);
+
+        assertEquals(300L, result);
+    }
+
+    @Test
+    public void checkVolumeTradedByEntityWhenNoEntityMatch() {
+
+        Rfq rfq1 = new Rfq();
+        rfq1.setEntityId(12345L);
+
+        String filePath = getClass().getResource("volume-traded-by-entity.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        VolumeTradedByEntityExtractor extractor = new VolumeTradedByEntityExtractor();
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq1, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastWeek);
+        Object result1 = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastMonth);
+        Object result2 = meta.get(RfqMetadataFieldNames.volumeTradedByEntityPastYear);
+
+
+        assertEquals(0L, result);
+        assertEquals(0L, result1);
+        assertEquals(0L, result2);
+    }
+
 }
